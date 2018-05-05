cbaf431f5372836bab6242cbebc8d9ff49409561
jee <jag918@nyu.edu>
2018-05-05 03:40:02 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,11 +21,11 @@ def first_element_with_array_methods(array)
   array.shift
 end
 
-def last_element_with_array_methods
+def last_element_with_array_methods(array)
   array.pop
 end
 
-def length_of_array
+def length_of_array(array)
   array.size 
 end
   
