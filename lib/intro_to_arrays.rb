879bdf2f27a3d9ac60a424a9488f7f9303f19c16
jee <jag918@nyu.edu>
2018-05-05 03:34:47 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,20 @@
+def instantiate_new_array
+  new_array=[]
+end
+def array_with_two_elements
+  new_array=[2,3]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+  
